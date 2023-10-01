c31dfc99b42c79a147856ef284e1e18e6703f635
Sunil Band <sunilband0007@gmail.com>
2023-10-01 10:36:27 +0530

    added waves

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {Providers} from "./providers";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <html lang="en" className='light'>
+    <body>
+      <Providers>
+        {children}
+      </Providers>
+    </body>
+  </html>
   )
 }
---
+++ components/Waves/Waves.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Waves.css'
+
+type Props = {}
+
+const Waves = (props: Props) => {
+  return (
+    
+<div className='h-screen w-screen absolute -z-0'>
+  {/*Hey! This is the original version
+of Simple CSS Waves*/}
+  <div className="header">
+    {/*Content before waves*/}
+    <div className="inner-header flex">
+      
+    </div>
+    {/*Waves Container*/}
+    <div>
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shapeRendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g className="parallax">
+          <use
+            xlinkHref="#gentle-wave"
+            x={48}
+            y={0}
+            fill="rgba(255,255,255,0.7"
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x={48}
+            y={3}
+            fill="rgba(255,255,255,0.5)"
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x={48}
+            y={5}
+            fill="rgba(255,255,255,0.3)"
+          />
+          <use xlinkHref="#gentle-wave" x={48} y={7} fill="#fff" />
+        </g>
+      </svg>
+    </div>
+    {/*Waves end*/}
+  </div>
+  {/*Header ends*/}
+  {/*Content starts*/}
+
+  {/*Content ends*/}
+</div>
+    
+    
+
+  )
+}
+
+export default Waves
