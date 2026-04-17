5e61bf822a3448007ee082d84b6084e3094560f1
Sunil Band <sunilband0007@gmail.com>
2026-04-17 09:49:31 +0530

    feat: short pwa

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,5 @@
-const CACHE_NAME = "task-app-v1";
-const urlsToCache = ["/", "/manifest.json"];
+self.addEventListener("install", () => self.skipWaiting());
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
